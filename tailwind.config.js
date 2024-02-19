e3c7306f838b32172fba98fa319717bc4003eaf7
jymbocala <jym.bocala@gmail.com>
2024-02-19 13:38:04 +1100

    feat: add custom theme configuration for daisyui plugin
    The commit adds a custom theme configuration for the daisyui plugin in the tailwind.config.js file. The custom theme includes primary, secondary, accent, neutral, base-100, success, warning, and error colors. This allows for customization of the UI appearance using the daisyui plugin.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,21 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#31485e",
+          "secondary": "#7eabe7",
+          "accent": "#f05656",
+          "neutral": "#030911",
+          "base-100": "#f2f7fd", //Base color of page, used for blank backgrounds
+          "success": "#87d039",
+          "warning": "#e2d562",
+          "error": "#ff6f6f",
+        },
+      }
+    ],
+  },
 }
 
