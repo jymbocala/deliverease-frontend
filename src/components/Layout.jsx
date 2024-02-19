a4170ec48e21eaed539c9af16254de7ca4c34cab
scg-code <samgifford.work@gmail.com>
2024-02-19 15:22:07 +1000

    Added hero section,  and installed a package called react-spring for an animation of Ease on page render

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Nav from "./Nav"
 import Footer from "./Footer"
+import Hero from "./Hero"
 
 
 export default function Layout() {
     return (
         <div className="site-wrapper">
             <Nav />
+            <Hero />
             <main>
                 <Outlet />
             </main>
