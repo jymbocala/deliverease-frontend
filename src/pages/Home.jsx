be1c45fe6917b190750d5f62411b23ae96e67c0d
scg-code <samgifford.work@gmail.com>
2024-02-20 09:51:32 +1000

    Added some effects and styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { useSpring, animated } from "react-spring";
+import { motion } from "framer-motion";
+import { IoIosArrowDown } from "react-icons/io";
+import { useInView } from "react-intersection-observer";
+
+const AnimatedArrow = () => {
+  return (
+    <div className="flex justify-center mt-8">
+      <ScrollLink to="keyFeatures" smooth={true} duration={500}>
+        <IoIosArrowDown className="text-accent animate-bounce text-4xl cursor-pointer" />
+      </ScrollLink>
+    </div>
+  );
+};
 
 const Home = () => {
+  const [ref1, inView1] = useInView({ triggerOnce: false });
+  const [ref2, inView2] = useInView({ triggerOnce: false });
+  const [ref3, inView3] = useInView({ triggerOnce: false });
+
   const props = useSpring({
     to: { x: 0 }, // Target rightward movement of 200 units (tweak as needed)
     from: { x: -200 }, // Start off-screen to the left
@@ -60,10 +77,18 @@ const Home = () => {
           </div>
         </div>
       </section>
-
+      <AnimatedArrow />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#31485e"
+          fill-opacity="0.8"
+          d="M0,224L60,208C120,192,240,160,360,138.7C480,117,600,107,720,138.7C840,171,960,245,1080,272C1200,299,1320,277,1380,266.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+      <div style={{ height: "100px" }}></div>
       {/* Features Section */}
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
+        <div id="keyFeatures" className="container px-5 pt-200 py-24 mx-auto">
           {/* Title */}
           <h1 className="text-3xl font-bold text-center mb-6">Key Features</h1>
           {/* Paragraph */}
@@ -72,8 +97,14 @@ const Home = () => {
             revolutionise your delivery operations.
           </p>
           {/* Features Section */}
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            {/* Feature 1 */}
+          <motion.div
+            ref={ref1}
+            className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: inView1 ? 1 : 0, x: inView1 ? 0 : -100 }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
+          >
+            {/* Feature 1 Icon */}
             <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden">
               <img
                 src="src/assets/images/map-image.png"
@@ -81,7 +112,7 @@ const Home = () => {
                 className="sm:w-48 sm:h-48 w-24 h-24 object-cover"
               />
             </div>
-
+            {/* Feature 1 Text */}
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 className="text-gray-900 text-xl title-font font-medium mb-2">
                 Effortless Delivery Location Management
@@ -92,8 +123,17 @@ const Home = () => {
                 streamlined delivery operations.
               </p>
             </div>
-          </div>
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+          </motion.div>
+
+          {/* Feature Block 2 */}
+          <motion.div
+            ref={ref2}
+            className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: inView2 ? 1 : 0, x: inView2 ? 0 : 100 }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
+          >
+            {/* Feature 2 Icon */}
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 className="text-gray-900 text-xl title-font font-medium mb-2">
                 Comprehensive Location Details
@@ -104,6 +144,7 @@ const Home = () => {
                 logistics.
               </p>
             </div>
+            {/* Feature 2 Image */}
             <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden">
               <img
                 src="src/assets/images/document-image.png"
@@ -111,8 +152,17 @@ const Home = () => {
                 className="sm:w-48 sm:h-48 w-24 h-24 object-cover"
               />
             </div>
-          </div>
-          <div className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
+          </motion.div>
+
+          {/* Feature Block 3 */}
+          <motion.div
+            ref={ref3}
+            className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: inView3 ? 1 : 0, x: inView3 ? 0 : -100 }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
+          >
+            {/* Feature 3 Icon */}
             <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0 overflow-hidden">
               <img
                 src="src/assets/images/dashboard-image.png"
@@ -120,18 +170,17 @@ const Home = () => {
                 className="sm:w-48 sm:h-48 w-24 h-24 object-cover"
               />
             </div>
+            {/* Feature 3 Text */}
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 className="text-gray-900 text-xl title-font font-medium mb-2">
                 User-Friendly Interface
               </h2>
               <p className="leading-relaxed text-base">
                 DeliverEase offers a user-friendly interface designed to
-                simplify the delivery management process, allowing users to
-                easily navigate, search, and organise delivery locations with
-                ease.
+                simplify the delivery management process...
               </p>
             </div>
-          </div>
+          </motion.div>
           <RouterLink to="/sign-up">
             <button className="flex mx-auto mt-20 text-white bg-primary border-0 py-2 px-8 focus:outline-none hover:bg-secondary rounded text-lg">
               Sign Up
