7351e971705eb0c4201438f22a9526decb76631f
scg-code <samgifford.work@gmail.com>
2024-02-19 14:32:32 +1000

    update navbar layout and styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,60 +1,65 @@
 import React from "react";
-import Logo from "/docs/images/deliverease-logo.png"; // Importing the PNG logo
+import Logo from "/docs/images/deliverease-logo.png"; 
 
 const Nav = () => {
-    return (
-        <div className="navbar navbar-custom" style={{ height: "70px" }}>
-            <div className="navbar-start">
-                {/* Dropdown */}
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-                    >
-                        <li>
-                            <a>Home</a>
-                        </li>
-                        <li>
-                            <a>Locations</a>
-                            <ul className="p-2">
-                                <li>
-                                    <a>All Locations</a>
-                                </li>
-                                <li>
-                                    <a>New Location</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a>Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <div className="navbar navbar-custom" style={{ height: "70px" }}>
+      <div className="navbar-start">
+        {/* Dropdown */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Locations</a>
+              <ul className="p-2">
+                <li>
+                  <a>All Locations</a>
+                </li>
+                <li>
+                  <a>New Location</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </div>
 
-                {/* Logo Container */}
-                <div className="logo-container">
-                    <img
-                        src={Logo}
-                        alt="DeliverEase Logo"
-                        style={{
-                            objectFit: "contain",
-                            maxHeight: "250px", // Adjust the logo height here
-                            marginLeft: "20px", // Adjust the logo margin here
-                        }}
-                    />
-                </div>
-            </div>
+        {/* Logo Container */}
+        <div className="logo-container">
+          <img
+            src={Logo}
+            alt="DeliverEase Logo"
+            style={{
+              objectFit: "contain",
+              maxHeight: "250px", // Adjust the logo height here
+              marginLeft: "20px", // Adjust the logo margin here
+            }}
+          />
+        </div>
+      </div>
 
             {/* Navbar Center (Hidden on smaller screens) */}
             <div className="navbar-center hidden lg:flex">
@@ -81,14 +86,17 @@ const Nav = () => {
                 </ul>
             </div>
 
-            {/* Navbar End */}
-            <div className="navbar-end">
-                <a className="btn primary" style={{ color: "white" }}>
-                    Sign In
-                </a>
-            </div>
-        </div>
-    );
+      {/* Navbar End */}
+      <div className="navbar-end">
+        <a
+          className="btn bg-primary hover:bg-secondary text-white transition-colors duration-200"
+          href="/login"
+        >
+          Sign In
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
