226c6a9dca76dac3e28ccf5153ab2bd74f9e36f9
jymbocala <jym.bocala@gmail.com>
2024-02-19 14:29:01 +1100

    feat: add routing and layout components to the app
    - Added a new component called Layout that wraps the main content of the app
    - Imported the necessary components from react-router-dom to set up routing
    - Updated the App component to use BrowserRouter and Routes for routing
    - Added routes for Home, Login, Contact, Locations, and Not Found pages
    - Set up nested routes within the Layout component
    - Commented out the Footer component for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
 
 function App() {
   return (
     <>
-      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<h1>Home</h1>} />
+            <Route path="login" element={<h1>Login page</h1>} />
+            <Route path="contact" element={<h1>Contact Page</h1>} />
+
+            {/* TODO: Set up authication to access routes below */}
+            <Route path="locations" element={<h1>All locations</h1>} />
+            <Route
+              path="locations/new"
+              element={<h1>Add new location page</h1>}
+            />
+            <Route
+              path="locations/:locationId"
+              element={<h1>One location page</h1>}
+            />
+
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
